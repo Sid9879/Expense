e64f826c0af83959bcf765d56b06c73a9849cc2c
Sid9879 <singhsiddharth1438@gmail.com>
2025-02-26 13:17:30 +0530

    protected

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ import Signup from './Pages/Signup'
 
 function App() {
     let [login, setlogin] = useState(false);
+   
   return (
     <>
      <BrowserRouter>
      <Navbar/>
      <Routes>
      <Route path="/" element={login===true ? <Home /> : <Navigate to="/login" />} />
-     <Route path="/login" element={ login === false ? <Login setlogin={setlogin}/> : <Navigate to = "/login" />} />
+     <Route path="/login" element={ login === false ? <Login setlogin={setlogin}/> : <Navigate to = "/" />} />
       <Route path= '/signup' element={<Signup/>}/>
      </Routes>
      </BrowserRouter>
