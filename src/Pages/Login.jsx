75e50aeedd7e8337038b19de236d7082d68538bc
Sid9879 <singhsiddharth1438@gmail.com>
2025-02-26 13:05:28 +0530

    new

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Login = ({ setLogin }) => {
+const Login = ({ setlogin }) => {
   let emailRef = useRef()
   let passwordRef = useRef()
   let navigate = useNavigate()
